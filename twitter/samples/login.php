46b440a9b5393190011583ae0ef387c096f7efa0
ewstevenson <ewsutllc@gmail.com>
2014-02-02 21:26:19 -0700

    added some sample

--- twitter/samples/login.php
+++ twitter/samples/login.php
@@ -1,3 +1,9 @@
 <?php
 
+function twitter_connection(){
+    // Connect to Twitter
+    $connection = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);    $content = $connection->post('statuses/update', array('status' => "$tweet"));
+    return $connection;
+}
+
 ?>
