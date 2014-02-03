ed8417ceb2e038a29e0a2a3f014a35bff4a22371
ewstevenson <ewsutllc@gmail.com>
2014-02-02 21:39:57 -0700

    added something;

--- twitter/samples/login.php
+++ twitter/samples/login.php
@@ -1,8 +1,14 @@
 <?php
 
+include_once('hehy.conf.php');
+
+
+print_r(twitter_connection());
+
+
 function twitter_connection(){
     // Connect to Twitter
-    $connection = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);    $content = $connection->post('statuses/update', array('status' => "$tweet"));
+    $connection = new TwitterOAuth($twitter_consumer_key, $twitter_consumer_secret, $twitter_access_token, $twitter_access_token_secret);
     return $connection;
 }
 
