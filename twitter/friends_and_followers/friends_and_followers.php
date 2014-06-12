1343e8c044d041cdf7d6af8a38175c01c2164e17
ewstevenson <ericwilliamstevenson@gmail.com>
2014-06-11 23:24:57 -0400

    renamed files and created follower monster sample

---
+++ twitter/friends_and_followers/friends_and_followers.php
@@ -0,0 +1,29 @@
+<?php
+
+function follow_user($bot, $userid) {
+	$post_url = 'https://api.twitter.com/1.1/friendships/create.json?user_id='.$userid.'&follow=true';
+	return $bot->post($post_url);
+}
+
+function unfollow_user($bot, $userid) {
+	$post_url = 'https://api.twitter.com/1.1/friendships/destroy.json?user_id=' . $userid ;
+	return $bot->post($post_url);
+
+}
+
+function get_suggested($bot) {
+	return $bot->get('https://api.twitter.com/1.1/users/suggestions/twitter.json');
+}
+
+function get_friend_ids($bot, $userid) {
+	$get_url = 'https://api.twitter.com/1.1/friends/ids.json?cursor=-1&user_id=' . $userid . '&count=1000';
+	return $bot->get($get_url);
+}
+
+function get_followers($bot, $userid, $cursor) {
+	$get_url = 'https://api.twitter.com/1.1/followers/list.json?cursor=' . $cursor . '&user_id=' . $userid;
+	return $bot->get($get_url);
+
+}
+
+?>
