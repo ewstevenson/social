5dbf0d11f1e9b622d1f185a9e481be74f17a6da7
ewstevenson <ericwilliamstevenson@gmail.com>
2015-11-03 21:08:56 -0500

    fixed it all.

--- twitter/samples/follow_monster.php
+++ twitter/samples/follow_monster.php
@@ -13,7 +13,7 @@ include_once('../friends_and_followers/friends_and_followers.php'); // INCLUDE T
 
 $twitter_bot = twitter_login(); // CREATE THE BOT
 $get_suggested_user_lists = get_suggested($twitter_bot); // RETURNS LIST OF 'SUGGESTED' TWITTER IDS
-#$friend_ids = get_friend_ids($twitter_bot, $owner_id); // RETURNS CURSORED LIST OF FRIENDS FOR PROVIDED TWITTER ID. THIS SHOULD BE YOURS. 
+$friend_ids = get_friend_ids($twitter_bot, '3255871182'); // RETURNS CURSORED LIST OF FRIENDS FOR PROVIDED TWITTER ID. THIS SHOULD BE YOURS. 
 
 
 /**
@@ -27,6 +27,8 @@ $get_suggested_user_lists = get_suggested($twitter_bot); // RETURNS LIST OF 'SUG
 #	$unfollow = unfollow_user($twitter_bot, $friend_id);
 #	print_r($unfollow);
 #	sleep(30);
+#               $unfollow = unfollow_user($twitter_bot, array_pop($friend_ids->{'ids'}));
+
 #}
 
 // die;
@@ -36,9 +38,11 @@ foreach ($get_suggested_user_lists as $user_list) {
 	#print_r($user_list);
 	foreach ($user_list as $users) {
 		foreach ($users as $user) { 
-			print_r($user);
+			#print_r($user);
 			print 'Following: '.$user->{'name'}." ".$user->{'id_str'}."\n";
 			$follow = follow_user($twitter_bot, $user->{'id_str'});
+			$unfollow = unfollow_user($twitter_bot, array_pop($friend_ids->{'ids'}));
+			$unfollow = unfollow_user($twitter_bot, array_pop($friend_ids->{'ids'}));
 			sleep(120);
 		}
 	}
